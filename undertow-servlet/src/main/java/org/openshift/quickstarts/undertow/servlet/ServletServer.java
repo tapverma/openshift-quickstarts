400054df92e8fd5da7086cbac1e0e32bd79339c2
tapverma <33484297+tapverma@users.noreply.github.com>
2019-04-17 17:21:38 +0530

    Update ServletServer.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World 2")
+                                    .addInitParam("message", "Hello World 3")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
